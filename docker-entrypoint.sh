7d6279fa956b1bb73fa501018122994b4b7e3b69
passer-b <der.passant.b@gmail.com>
2017-09-10 21:21:54 +0900

    support TZ environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -n "${TZ}" -a -z "$(echo ${TZ} | grep -v '\.\.')" -a -f /usr/share/zoneinfo/${TZ} ]; then
+	echo "set timezone ${TZ} by TZ environment variable."
+	cp /usr/share/zoneinfo/${TZ} /etc/localtime
+fi
+
 if [ "$1" = 'airflow' ]; then
 	airflow initdb
 fi
